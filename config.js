13c9a361dafa9c84bd754275090db202cf658b85
arpit <arpitpathak97@gmail.com>
2020-11-17 02:35:20 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'Panacea', // Required
+    author: 'Arpit Pathak', // Required
+    description: 'Tech and More',
+    primaryColor: '#30308a', // Required
+    showHeaderImage: true,
+    showShareButtons: true,
+    postsPerPage: 5, // Required
+    social: {
+        website: '#',
+        github: 'https://github.com/hold7door',
+        twitter: 'https://twitter.com/AP_zahlen',
+        linkedin: 'https://www.linkedin.com/in/arpit-pathak-642573175/',
+    },
+    pathPrefix: '/panacea',
+    siteUrl: 'https://hold7door.github.io/panacea/',
+};
---
+++ src/pages/welcome-to-the-blog/index.md
@@ -0,0 +1,11 @@
+---
+title: Welcome to the Blog!
+date: '2020-11-17'
+featuredImage: './featured.png'
+---
+
+Welcome to the new blog, I hope you enjoy your stay!
+
+<!-- end -->
+
+> _"A journey of a thousand miles begins with a single step. "_ _--Lao Tzu_
